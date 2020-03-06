5e231c98a7a6c0174c60b21b5de54fad6cd89032
Adrien <Adnn@users.noreply.github.com>
2020-03-06 18:33:10 +0100

    WIP further: needs to try to blend the results

--- src/app/shmurp/Bloom.h
+++ src/app/shmurp/Bloom.h
@@ -18,8 +18,16 @@ public:
     Bloom(Size<2, GLsizei> aResolution)
 
     {
+        glProgramUniform1i(mHBlurProgram,
+                           glGetUniformLocation(mHBlurProgram, "inputTexture"),
+                           0);
+        glProgramUniform1i(mVBlurProgram,
+                           glGetUniformLocation(mVBlurProgram, "inputTexture"),
+                           1);
+
         allocateStorage(mTextureScene, GL_RGBA8, aResolution);
         allocateStorage(mTexturePing,  GL_RGBA8, aResolution);
+        allocateStorage(mTexturePong,  GL_RGBA8, aResolution);
 
         attachImage(mSceneFB, mTextureScene, GL_COLOR_ATTACHMENT0, 0);
         attachImage(mSceneFB, mTexturePing,  GL_COLOR_ATTACHMENT1, 0);
@@ -29,6 +37,9 @@ public:
             unsigned int attachments[2] = {GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1};
             glDrawBuffers(2, attachments);
         }
+
+        attachImage(mPingFB, mTexturePing, GL_COLOR_ATTACHMENT0, 0);
+        attachImage(mPongFB, mTexturePong, GL_COLOR_ATTACHMENT0, 0);
     }
 
     [[nodiscard]] auto bindFramebuffer()
@@ -36,9 +47,27 @@ public:
         return bind_guard{mSceneFB};
     }
 
+    void bloom(int aPasses)
+    {
+        glBindVertexArray(mScreenQuad.mVertexArray);
+
+        glActiveTexture(GL_TEXTURE0);
+        bind(mTexturePing);
+
+        glActiveTexture(GL_TEXTURE1);
+        bind(mTexturePong);
+
+        for (int pass = 0; pass != 2*aPasses; ++pass)
+        {
+            glUseProgram(pass%2 ? mVBlurProgram : mHBlurProgram);
+            glBindFramebuffer(GL_FRAMEBUFFER, pass%2 ? mPingFB : mPongFB);
+            glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+        }
+    }
+
     void drawResult()
     {
-        glBindVertexArray(gScreenQuad.mVertexArray);
+        glBindVertexArray(mScreenQuad.mVertexArray);
 
         glActiveTexture(GL_TEXTURE0);
         bind(mTextureScene);
@@ -56,14 +85,24 @@ private:
             {GL_VERTEX_SHADER, gScreenVertex},
             {GL_FRAGMENT_SHADER, gScreenFragment}})};
 
+    Program mHBlurProgram{makeLinkedProgram({
+            {GL_VERTEX_SHADER, gScreenVertex},
+            {GL_FRAGMENT_SHADER, gHBlurFragment}})};
+    Program mVBlurProgram{makeLinkedProgram({
+            {GL_VERTEX_SHADER, gScreenVertex},
+            {GL_FRAGMENT_SHADER, gVBlurFragment}})};
+
     Texture mTextureScene{GL_TEXTURE_2D};
     Texture mTexturePing{GL_TEXTURE_2D};
+    Texture mTexturePong{GL_TEXTURE_2D};
 
     FrameBuffer mSceneFB;
+    FrameBuffer mPingFB;
+    FrameBuffer mPongFB;
 
     // Cannot be static: the application must be started before starting to load
     // VOAs and VBOs
-    const VertexSpecification gScreenQuad{make_ScreenQuad()};
+    const VertexSpecification mScreenQuad{make_ScreenQuad()};
 };
 
 
--- src/app/shmurp/BloomShaders.h
+++ src/app/shmurp/BloomShaders.h
@@ -1,3 +1,6 @@
+//
+// COMPOSING RESULT
+//
 static const char* gScreenVertex = R"#(
 #version 400
 
@@ -31,3 +34,57 @@ void main(void)
     //            + texture(bloomTexture, ex_TexCoords);
 }
 )#";
+
+
+//
+// BLURRING
+//
+static const char* gHBlurFragment = R"#(
+#version 400
+
+in vec2 ex_TexCoords;
+out vec4 out_Color;
+
+uniform sampler2D inputTexture;
+vec2 textureSize = textureSize(inputTexture, 0);
+
+float weights[] = float[](0.4, 0.2, 0.1, 0.08, 0.05, 0.05, 0.05, 0.03, 0.02, 0.01);
+
+void main(void)
+{
+    out_Color = weights[0] * texture(inputTexture, ex_TexCoords);
+    vec2 texOffset = vec2(1.0/textureSize.x, 0.);
+
+    for(int i=1; i<weights.length(); ++i)
+    {
+        out_Color += weights[i] * texture(inputTexture, ex_TexCoords - (i*texOffset));
+        out_Color += weights[i] * texture(inputTexture, ex_TexCoords + (i*texOffset));
+    }
+}
+)#";
+
+static const char* gVBlurFragment = R"#(
+#version 400
+
+in vec2 ex_TexCoords;
+out vec4 out_Color;
+
+uniform sampler2D inputTexture;
+vec2 textureSize = textureSize(inputTexture, 0);
+
+//float weights[4] = float[](0.3, 0.2, 0.1, 0.05);
+float weights[] = float[](0.4, 0.2, 0.1, 0.08, 0.05, 0.05, 0.05, 0.03, 0.02, 0.01);
+
+void main(void)
+{
+    out_Color = weights[0] * texture(inputTexture, ex_TexCoords);
+    vec2 texOffset = vec2(0., 1.0/textureSize.x);
+
+    for(int i=1; i<weights.length(); ++i)
+    {
+        out_Color += weights[i] * texture(inputTexture, ex_TexCoords - (i*texOffset));
+        out_Color += weights[i] * texture(inputTexture, ex_TexCoords + (i*texOffset));
+    }
+}
+)#";
+

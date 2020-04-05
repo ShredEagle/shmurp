66b21ab125d5489e08d425679ca63a8dbe4e011f
Adrien <Adnn@users.noreply.github.com>
2020-04-05 13:26:09 +0200

    Periodic first triggers instantly, implement Rythm

--- src/app/shmurp/Utils/Periodic.h
+++ src/app/shmurp/Utils/Periodic.h
@@ -10,6 +10,8 @@ namespace ad {
 typedef double timet;
 
 /// TODO make it std::duration based
+// \brief A periodic tick, notably allowing to trigger events at regulare interval
+// \important The first tick happens instantly (not waiting one period)
 class Periodic
 {
 public:
@@ -30,11 +32,11 @@ private:
 
 constexpr std::size_t Periodic::countEvents(timet aDelta)
 {
-    std::size_t result=0;
+    std::size_t result = 0;
     mRemainingTime += aDelta;
 
     /// TODO once integral, go modulo
-    while((mRemainingTime - mPeriod) >= 0.)
+    while(mRemainingTime >= 0.)
     {
         mRemainingTime -= mPeriod;
         ++result;
@@ -42,15 +44,16 @@ constexpr std::size_t Periodic::countEvents(timet aDelta)
     return result;
 }
 
+
 template <class T_functor, class... VT_args>
 void Periodic::forEachEvent(timet aDelta, const T_functor & aFunctor, VT_args &&... aArgs)
 {
     mRemainingTime += aDelta;
 
-    while((mRemainingTime - mPeriod) >= 0.)
+    while(mRemainingTime >= 0.)
     {
-        mRemainingTime -= mPeriod;
         aFunctor(mRemainingTime, std::forward<VT_args>(aArgs)...);
+        mRemainingTime -= mPeriod;
     }
 }
 
---
+++ src/app/shmurp/Utils/Rythm.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "Periodic.h"
+
+#include <gsl/gsl>
+
+#include <algorithm>
+#include <vector>
+
+
+namespace ad {
+
+class Rythm
+{
+public:
+    explicit Rythm(timet aPeriod, int aDuration) :
+        mMetronom(aPeriod),
+        mDuration(aDuration)
+    {}
+
+    Rythm & note(int aTime);
+
+    template <class T_functor, class... VT_args>
+    void forEachEvent(timet aDelta, const T_functor & aFunctor, VT_args &&... aArgs);
+
+private:
+    Periodic mMetronom;
+    int mDuration;
+    std::vector<int> mScore{};
+    int mCurrentNote{0};
+};
+
+
+Rythm & Rythm::note(int aTime)
+{
+    Expects( (aTime >= 0) && (aTime < mDuration) );
+    mScore.push_back(aTime);
+    return *this;
+}
+
+
+template <class T_functor, class... VT_args>
+void Rythm::forEachEvent(timet aDelta, const T_functor & aFunctor, VT_args &&... aArgs)
+{
+    mMetronom.forEachEvent(aDelta, [&aFunctor, this](timet aRemainingTime, VT_args &&... aArgs)
+    {
+        if (std::find(mScore.begin(), mScore.end(), mCurrentNote) != mScore.end())
+        {
+            aFunctor(aRemainingTime, std::forward<VT_args>(aArgs)...);
+        }
+        if (++mCurrentNote == mDuration)
+        {
+            mCurrentNote = 0;
+        };
+    }, std::forward<VT_args>(aArgs)...);
+}
+
+
+} // namespace ad
